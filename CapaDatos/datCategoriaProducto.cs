f7f7bae897bf0853fcff3d376d4969dc7a75a753
Tu Nombre <tu.email@example.com>
2025-07-07 22:46:02 -0500

    LAS 3 PRIMERAS CAPAS HECHAS

---
+++ CapaDatos/datCategoriaProducto.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class datCategoriaProducto
+    {
+        private static readonly datCategoriaProducto _instancia = new datCategoriaProducto();
+        public static datCategoriaProducto Instancia => _instancia;
+
+        public List<entCategoriaProducto> Listar()
+        {
+            SqlCommand cmd = null;
+            List<entCategoriaProducto> lista = new List<entCategoriaProducto>();
+            try
+            {
+                SqlConnection cn = Conexion.Instancia.Conectar();
+                cmd = new SqlCommand("spListarCategoriaProducto", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    entCategoriaProducto cat = new entCategoriaProducto
+                    {
+                        CategoriaProductoID = Convert.ToInt32(dr["CategoriaProductoID"]),
+                        Nombre = dr["Nombre"].ToString(),
+                        Descripcion = dr["Descripcion"].ToString()
+                    };
+                    lista.Add(cat);
+                }
+            }
+            catch (Exception e) { throw e; }
+            finally { cmd.Connection.Close(); }
+            return lista;
+        }
+
+        public bool Insertar(entCategoriaProducto c)
+        {
+            SqlCommand cmd = null;
+            bool inserta = false;
+            try
+            {
+                SqlConnection cn = Conexion.Instancia.Conectar();
+                cmd = new SqlCommand("spInsertarCategoriaProducto", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Nombre", c.Nombre);
+                cmd.Parameters.AddWithValue("@Descripcion", c.Descripcion);
+                cn.Open();
+                inserta = cmd.ExecuteNonQuery() > 0;
+            }
+            catch (Exception e) { throw e; }
+            finally { cmd.Connection.Close(); }
+            return inserta;
+        }
+
+        public bool Editar(entCategoriaProducto c)
+        {
+            SqlCommand cmd = null;
+            bool edita = false;
+            try
+            {
+                SqlConnection cn = Conexion.Instancia.Conectar();
+                cmd = new SqlCommand("spEditarCategoriaProducto", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@CategoriaProductoID", c.CategoriaProductoID);
+                cmd.Parameters.AddWithValue("@Nombre", c.Nombre);
+                cmd.Parameters.AddWithValue("@Descripcion", c.Descripcion);
+                cn.Open();
+                edita = cmd.ExecuteNonQuery() > 0;
+            }
+            catch (Exception e) { throw e; }
+            finally { cmd.Connection.Close(); }
+            return edita;
+        }
+
+        public bool Eliminar(int id)
+        {
+            SqlCommand cmd = null;
+            bool elimina = false;
+            try
+            {
+                SqlConnection cn = Conexion.Instancia.Conectar();
+                cmd = new SqlCommand("spEliminarCategoriaProducto", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@CategoriaProductoID", id);
+                cn.Open();
+                elimina = cmd.ExecuteNonQuery() > 0;
+            }
+            catch (Exception e) { throw e; }
+            finally { cmd.Connection.Close(); }
+            return elimina;
+        }
+    }
+}
--- CapaLogica/logCategoriaProducto.cs
+++ CapaLogica/logCategoriaProducto.cs
@@ -8,5 +8,12 @@ namespace CapaLogica
 {
     public class logCategoriaProducto
     {
+        private static readonly logCategoriaProducto _instancia = new logCategoriaProducto();
+        public static logCategoriaProducto Instancia => _instancia;
+
+        public List<entCategoriaProducto> Listar() => datCategoriaProducto.Instancia.Listar();
+        public void Insertar(entCategoriaProducto c) => datCategoriaProducto.Instancia.Insertar(c);
+        public void Editar(entCategoriaProducto c) => datCategoriaProducto.Instancia.Editar(c);
+        public void Eliminar(int id) => datCategoriaProducto.Instancia.Eliminar(id);
     }
 }
