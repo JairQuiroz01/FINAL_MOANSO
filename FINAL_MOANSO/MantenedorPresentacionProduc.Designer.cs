c0d9591ab96a81e82b830d298b560ecd20e0a353
JairQuiroz01 <junior.agosto.rc@gmail.com>
2025-07-08 07:41:09 -0500

    funciones a PresentacionProduc

--- FINAL_MOANSO/MantenedorPresentacionProduc.Designer.cs
+++ FINAL_MOANSO/MantenedorPresentacionProduc.Designer.cs
@@ -43,60 +43,93 @@ namespace FINAL_MOANSO
             ((System.ComponentModel.ISupportInitialize)(this.picImagen)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPresentacion)).BeginInit();
             this.SuspendLayout();
-
+            // 
             // lblNombre
+            // 
             this.lblNombre.Location = new System.Drawing.Point(20, 20);
+            this.lblNombre.Name = "lblNombre";
             this.lblNombre.Size = new System.Drawing.Size(80, 20);
+            this.lblNombre.TabIndex = 0;
             this.lblNombre.Text = "Nombre:";
-
-            // txtNombre
-            this.txtNombre.Location = new System.Drawing.Point(110, 20);
-            this.txtNombre.Size = new System.Drawing.Size(200, 20);
-
+            // 
             // lblDescripcion
+            // 
             this.lblDescripcion.Location = new System.Drawing.Point(20, 60);
+            this.lblDescripcion.Name = "lblDescripcion";
             this.lblDescripcion.Size = new System.Drawing.Size(80, 20);
+            this.lblDescripcion.TabIndex = 2;
             this.lblDescripcion.Text = "Descripción:";
-
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(110, 20);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(200, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
             // txtDescripcion
+            // 
             this.txtDescripcion.Location = new System.Drawing.Point(110, 60);
             this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
             this.txtDescripcion.Size = new System.Drawing.Size(300, 60);
-
+            this.txtDescripcion.TabIndex = 3;
+            // 
             // btnSeleccionarImagen
+            // 
             this.btnSeleccionarImagen.Location = new System.Drawing.Point(450, 20);
+            this.btnSeleccionarImagen.Name = "btnSeleccionarImagen";
             this.btnSeleccionarImagen.Size = new System.Drawing.Size(130, 30);
+            this.btnSeleccionarImagen.TabIndex = 4;
             this.btnSeleccionarImagen.Text = "Seleccionar Imagen";
-
+            // 
             // picImagen
+            // 
+            this.picImagen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.picImagen.Location = new System.Drawing.Point(600, 20);
+            this.picImagen.Name = "picImagen";
             this.picImagen.Size = new System.Drawing.Size(150, 100);
-            this.picImagen.SizeMode = PictureBoxSizeMode.Zoom;
-            this.picImagen.BorderStyle = BorderStyle.FixedSingle;
-
+            this.picImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picImagen.TabIndex = 5;
+            this.picImagen.TabStop = false;
+            // 
             // btnRegistrar
+            // 
             this.btnRegistrar.Location = new System.Drawing.Point(90, 140);
+            this.btnRegistrar.Name = "btnRegistrar";
             this.btnRegistrar.Size = new System.Drawing.Size(90, 30);
+            this.btnRegistrar.TabIndex = 6;
             this.btnRegistrar.Text = "Registrar";
-
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
             // btnModificar
+            // 
             this.btnModificar.Location = new System.Drawing.Point(200, 140);
+            this.btnModificar.Name = "btnModificar";
             this.btnModificar.Size = new System.Drawing.Size(90, 30);
+            this.btnModificar.TabIndex = 7;
             this.btnModificar.Text = "Modificar";
-
+            // 
             // btnInhabilitar
+            // 
             this.btnInhabilitar.Location = new System.Drawing.Point(310, 140);
+            this.btnInhabilitar.Name = "btnInhabilitar";
             this.btnInhabilitar.Size = new System.Drawing.Size(90, 30);
+            this.btnInhabilitar.TabIndex = 8;
             this.btnInhabilitar.Text = "Inhabilitar";
-
+            // 
             // dgvPresentacion
-            this.dgvPresentacion.Location = new System.Drawing.Point(20, 190);
-            this.dgvPresentacion.Size = new System.Drawing.Size(730, 200);
-            this.dgvPresentacion.ReadOnly = true;
+            // 
             this.dgvPresentacion.AllowUserToAddRows = false;
-            this.dgvPresentacion.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-
-            // Form
+            this.dgvPresentacion.Location = new System.Drawing.Point(20, 190);
+            this.dgvPresentacion.Name = "dgvPresentacion";
+            this.dgvPresentacion.ReadOnly = true;
+            this.dgvPresentacion.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPresentacion.Size = new System.Drawing.Size(730, 200);
+            this.dgvPresentacion.TabIndex = 9;
+            // 
+            // MantenedorPresentacionProduc
+            // 
             this.ClientSize = new System.Drawing.Size(784, 411);
             this.Controls.Add(this.lblNombre);
             this.Controls.Add(this.txtNombre);
@@ -108,12 +141,13 @@ namespace FINAL_MOANSO
             this.Controls.Add(this.btnModificar);
             this.Controls.Add(this.btnInhabilitar);
             this.Controls.Add(this.dgvPresentacion);
+            this.Name = "MantenedorPresentacionProduc";
             this.Text = "Mantenedor Presentación de Producto";
-
             ((System.ComponentModel.ISupportInitialize)(this.picImagen)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPresentacion)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
